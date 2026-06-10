05e620af8f258c7bba1b6086771e943e96e5a16e
bhargavmane1802 <bhargavmane18@gmail.com>
2026-06-10 13:24:34 +0530

    for mail servive added gmail.apis

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,26 +1,53 @@
-import { Resend } from "resend";
-const resend = new Resend("process.env.RESEND_API");
+import { google } from "googleapis"
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN
+});
 export const sendVerificationEmail = async (
-    email,
+    recipientEmail,
     token
 ) => {
-    const url =
-        `${process.env.BACKEND_URL}/user/verify/${token}`;
-
-        const { data, error } = await resend.emails.send({
-    from:process.env.EMAIL_USER,
-    to: [email],
-    subject: "Verify your email",
-    html:  `
-            <h1>Email Verification</h1>
-            <p>
-                Click the link below:
-            </p>
-
-            <a href="${url}">
-                Verify Account
-            </a>
-        `,
+    const url =`${process.env.BACKEND_URL}/user/verify/${token}`;
+  const gmail = google.gmail({
+    version: "v1",
+    auth: oauth2Client
   });
-};
 
+  const email = [
+  `To: ${recipientEmail}`,
+  "Subject: Verify Email",
+  "MIME-Version: 1.0",
+  "Content-Type: text/html; charset=UTF-8",
+  "",
+  `
+    <h1>Email Verification</h1>
+    <p>Click the link below:</p>
+    <a href="${url}">Verify Account</a>
+  `
+].join("\n");
+
+  const encodedMessage = Buffer.from(email)
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+
+  try {
+  await gmail.users.messages.send({
+    userId: "me",
+    requestBody: {
+      raw: encodedMessage
+    }
+  });
+
+  console.log("Email sent");
+} catch (error) {
+  console.error(
+    error.response?.data || error.message || error
+  );
+}
+};
